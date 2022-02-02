3a5e21fd06212afe64fd633896267d8c52fa0e4f
sunadoi <y.sunadoi@gmail.com>
2022-02-02 18:39:34 +0900

    use chan

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"sync"
 	"time"
 )
 
@@ -14,18 +13,27 @@ func main() {
 	// time.Sleep(time.Second * 5) // これを書かないとメインgoroutineが終了して、他のgoroutineの終了を待たずにプログラムが終了する
 
 	// 代わりにWaitGroupを使ってgoroutineの終了を待つ
-	var wg sync.WaitGroup
-	wg.Add(1)
+	// var wg sync.WaitGroup
+	// wg.Add(1)
 
-	go func() {
-		defer wg.Done()
-		getLuckyNum()
-	}()
+	// go func() {
+	// 	defer wg.Done()
+	// 	getLuckyNum()
+	// }()
 
-	wg.Wait()
+	// wg.Wait()
+
+	// チャネルを使って送受信を行う
+	c := make(chan int)
+	go getLuckyNum(c)
+
+	num := <-c
+	fmt.Printf("Today's your lucky number is %d!\n", num)
+
+	close(c)
 }
 
-func getLuckyNum() {
+func getLuckyNum(c chan<- int) {
 	fmt.Println("...")
 
 	// 占いにかかる時間はランダム
@@ -33,5 +41,6 @@ func getLuckyNum() {
 	time.Sleep(time.Duration(rand.Intn(3000)) * time.Millisecond)
 
 	num := rand.Intn(10)
-	fmt.Printf("Today's your lucky number is %d!\n", num)
+	// fmt.Printf("Today's your lucky number is %d!\n", num)
+	c <- num
 }
